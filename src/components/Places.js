736574476f8427011441a9f507ede0a150205b7a
Paweł Dzik <p.dzik1@wp.pl>
2023-01-24 19:28:26 +0100

    places & routes

--- src/components/Places.js
+++ src/components/Places.js
@@ -11,7 +11,19 @@ function Places() {
      <h1>Popular places</h1>
      <p>Guided tours shows you real Kyiv</p>
 
-     <PlacesData />
+     <PlacesData
+      heading='Independence Square'
+      text='The Independence Square, or Maidan Nezalezhnosti, is the central square in Kiev, the main and the most beautiful one. Parades, concerts, festivals and other city arrangements and holidays take place on this square. It contains six fountains, Independence Column and artificial waterfall. The Independence Square has much to offer: the huge building with the tower and chimes, the Trade Union Association Office and other attractions. The left side of the Independence square is covered with granite. The splendid panoramic view of Old Kiev opens from the upper floors of the square buildings.'
+      img1='https://img.itinari.com/activity/images/original/28008abe-6c2d-4123-99cb-254aa3796d75-istock-1134658793.jpg?ch=DPR&dpr=2.625&w=1200&h=800&s=3081e51a2c2f97ad632c9573e64d2428'
+      img2='https://ukrainetrek.com/blog/wp-content/uploads/2014/09/restored-independence-square-kyiv-ukraine-7.jpg'
+     />
+
+     <PlacesData
+      heading='Golden Gate'
+      text='The Golden Gate of Kyiv was the main gate in the 11th century fortifications of Kiev, the capital of Kievan Rus. It was named in imitation of the Golden Gate of Constantinople. The structure was dismantled in the Middle Ages, leaving few vestiges of its existence.'
+      img1={GoldenGate}
+      img2={GoldenGateStatue}
+     />
     </div>
   )
 }
--- src/components/PlacesData.js
+++ src/components/PlacesData.js
@@ -1,20 +1,17 @@
 import React from 'react'
 import classes from './Places.module.css'
 
-import GoldenGate from '../assets/images/golden-gate.jpg'
-import GoldenGateStatue from '../assets/images/golden-gate-statue.jpg.webp'
-
-function PlacesData() {
+function PlacesData(props) {
   return (
    <div className={classes['first-description']}>
    <div className={classes['description-text']}>
-     <h2>Golden Gate</h2>
-     <p>he Golden Gate of Kyiv was the main gate in the 11th century fortifications of Kiev, the capital of Kievan Rus'. It was named in imitation of the Golden Gate of Constantinople. The structure was dismantled in the Middle Ages, leaving few vestiges of its existence.</p>
+     <h2>{props.heading}</h2>
+     <p>{props.text}</p>
    </div>
 
    <div className={classes['image']}>
-     <img alt='golden gate kyiv' src={GoldenGate}/>
-     <img alt='golden gate kyiv' src={GoldenGateStatue}/>
+     <img alt='golden gate kyiv' src={props.img1}/>
+     <img alt='golden gate kyiv' src={props.img2}/>
    </div>
   </div>
   )
